d0e1c1d89ac76cdb521a0968578be7e7c2422f2b
Jeff Johns <phpfunk@gmail.com>
2010-03-09 20:32:27 -0500

    Added comments to code

--- bitly.php
+++ bitly.php
@@ -1,4 +1,10 @@
 <?php
+/**
+* Simple PHP Bit.ly API Wrapper Class.
+*
+* @author Jeff Johns <phpfunk@gmail.com>
+* @license MIT License
+*/
 class Bitly {
 
   public $api_key   =   NULL;
@@ -7,16 +13,46 @@ class Bitly {
   
   protected static $endpoint = 'http://api.bit.ly';
 
+  /**
+  * Magic method to request call for any method called
+  * that does not exist in the object.
+  *
+  * @param  string  $method   method called
+  * @param  array   $args     array of arguments
+  *
+  * @return array
+  */
   public function __call($method, $args)
   {
     return self::call($method, $args, "object");
   }
   
+  /**
+  * Magic method to request call for any method called
+  * that does not exist. This is the static version.
+  * Only available in PHP 5.3.0+
+  *
+  * @param  string  $method   method called
+  * @param  array   $args     array of arguments
+  *
+  * @return array
+  */
   public function __callStatic($method, $args)
   {
     return self::call($method, $args, "static");
   }
   
+  /**
+  * Called when you call a method that doesn't exist.
+  * Both the static and OO methods call this method
+  * to send the API requests to bit.ly.
+  *
+  * @param  string  $method   method called
+  * @param  array   $args     array of arguments
+  * @param  string  $type     type of call (object || static)
+  *
+  * @return array
+  */
   protected function call($method, $args, $type)
   {
     $method         =   strtolower($method);
@@ -54,6 +90,15 @@ class Bitly {
     return $res;
   }
   
+  /**
+  * General method to get data from the returned
+  * result. You can call any key from the results
+  * object returned from the API call.
+  *
+  * @param  string  $what : What you want to be returned (EX: shortUrl)
+  *
+  * @return bool || string || array
+  */
   public function get($what)
   {
     $method = 'get_' . $what;
@@ -69,11 +114,22 @@ class Bitly {
     return (isset($this->res->results->$key->$what)) ? $this->res->results->$key->$what : false;
   }
   
+  /**
+  * Returns a string with the error code and message
+  *
+  * @return string
+  */
   protected function get_error()
   {
     return 'Error Number (' . $this->res->errorCode . '): ' . $this->res->errorMessage;
   }
   
+  /**
+  * Returns the first key, where applicable under
+  * the results object returned from the API.
+  *
+  * @return bool || string
+  */
   protected function get_key()
   {
     foreach ((array) $this->res->results as $key => $arr) {
@@ -86,6 +142,12 @@ class Bitly {
     }
   }
   
+  /**
+  * Returns all (if any) referrers by site/path => total.
+  * And total number of referrers.
+  *
+  * @return array
+  */
   protected function get_referrers()
   {
     $referrers = (array) $this->res->results->referrers;
@@ -101,16 +163,39 @@ class Bitly {
     return $tmp;
   }
   
+  /**
+  * Returns a boolean if there is an error or not
+  *
+  * @return bool
+  */
   public function is_error()
   {
     return ($this->res->errorCode > 0 || strtolower($this->res->statusCode) != "ok") ? true : false;
   }
   
+  /**
+  * Returns boolean if the string is json or not
+  *
+  * @param  string  $str  String to be evaluated
+  *
+  * @return bool
+  */
   protected function is_json($str)
   {
     return (substr(trim($str), 0, 1) == '<') ? false : true;
   }
   
+  /**
+  * Since this class uses PHP's Simple XML it creates
+  * an object different than the json returned object.
+  * This method normalizes the XMl data to be returned
+  * in the same pattern as the JSON result would.
+  *
+  * @param  mixed   $obj      The xml object result from API call
+  * @param  string  $method   The method called
+  *
+  * @return mixed
+  */
   protected static function normalize($obj, $method)
   {
     $methods = array('errors','shorten','info');
@@ -143,6 +228,13 @@ class Bitly {
     return $obj;
   }
   
+  /**
+  * Remove CDATA tags from XML
+  *
+  * @param  string   $str The string to remove CDATA from
+  *
+  * @return string
+  */
   protected static function remove_cdata($str)
   {
     return preg_replace('#<!\[CDATA\[(.*?)\]\]>#s', "$1", $str);
